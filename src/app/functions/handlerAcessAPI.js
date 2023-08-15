34adf44e94f7123e67bebaf7d4ed2d737abdb0a9
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-15 15:03:41 -0300

    inserindo lista

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,20 @@
 'use server'
 
-const getUserAuthenticated = (user) => {
+const lista = [
+    {
+        name: "Anne",
+        email: "anne@gmail.com",
+        password: "123",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
+    }
+]
+
+const getUserAuthenticated = (user) => {
+    
 }
 
-const getUsers = () =>{
-        
+const getUsers = () => {
+
 }
 export { getUsers, getUserAuthenticated };
