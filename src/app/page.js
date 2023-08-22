530d82d088c9f36ceceb3603997adb303b51ea91
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-08-22 15:55:27 -0300

    consertando o import 'react-toastify/dist/ReactToastify.css';

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 import handlerAcessUser from "./functions/handlerAcess"
 import { useRouter } from "next/navigation";
 import './globals.css';
-import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/react-toastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
   const [user, setUser] = useState({
