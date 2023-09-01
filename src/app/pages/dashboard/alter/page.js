63bb628443b39809c84ebffb155d4802b3c29e90
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-09-01 19:26:02 -0300

    criei as páginas de Alterar e Registrar no Dashboard

---
+++ src/app/pages/dashboard/alter/page.js
@@ -0,0 +1,9 @@
+export default async function AlterDashboard() {
+    
+
+    return (
+        <div>
+         
+        </div>
+    )
+}
---
+++ src/app/pages/dashboard/register/page.js
@@ -0,0 +1,9 @@
+export default async function RegisterDashboard() {
+    
+
+    return (
+        <div>
+         
+        </div>
+    )
+}
