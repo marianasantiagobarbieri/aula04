152c8ae1aad0a8a972b9c59e13983d6107507a23
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-09-03 00:03:54 -0300

    criação do formulário de Register e Alter

--- src/app/pages/dashboard/alter/page.js
+++ src/app/pages/dashboard/alter/page.js
@@ -1,9 +1,36 @@
-export default async function AlterDashboard() {
-    
+export const metadata = {
+    title: 'Alter user',
+    description: 'Project for class PTAC'
+}
 
+
+export default async function AlterDashboard() {
     return (
-        <div>
-         
+        <div className='LoginBox'>
+            <div className='LoginContainer'>
+                <h1>Alterar Login</h1>
+                <form /*onSubmit={handlerLogin}*/ className='LoginForm'>
+                    <input
+                        className='LoginInput'
+                        placeholder='Nome:'
+                        type="nome">
+                    </input>
+                    <span className='SpamAlter'></span>
+                    <input
+                        className='LoginInput'
+                        placeholder='E-mail:'
+                        type="email">
+                    </input>
+                    <span className='SpamAlter'></span>
+                    <input
+                        className='LoginInput'
+                        placeholder='Senha:'
+                        type='password'>
+                    </input>
+                    <span className='SpamAlter'></span>
+                    <button className='LoginBotaoAlterar'>Alterar</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/app/pages/dashboard/register/page.js
+++ src/app/pages/dashboard/register/page.js
@@ -7,11 +7,33 @@ export const metadata = {
 
 
 export default async function RegisterDashboard() {
-    
 
     return (
-        <div>
-         <h1>RegisterDashboard</h1>
+        <div className='LoginBox'>
+            <div className='LoginContainer'>
+                <h1>Registrar Login</h1>
+                <form /*onSubmit={handlerLogin}*/ className='LoginForm'>
+                    <input
+                        className='LoginInput'
+                        placeholder='Nome:'
+                        type="nome">
+                    </input>
+                    <span className='SpamRegister'></span>
+                    <input
+                        className='LoginInput'
+                        placeholder='E-mail:'
+                        type="email">
+                    </input>
+                    <span className='SpamRegister'></span>
+                    <input
+                        className='LoginInput'
+                        placeholder='Senha:'
+                        type='password'>
+                    </input>
+                    <span className='SpamRegister'></span>
+                    <button className='LoginBotaoCRegistrar'>Registrar</button>
+                </form>
+            </div>
         </div>
     )
 }
