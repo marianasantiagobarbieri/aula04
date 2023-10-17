fba030ecb29761510a9ee7cf74c211602a3540bd
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-10-17 16:14:04 -0300

    fazendo o get p/ pegar os usuários cadastrados

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -3,7 +3,7 @@ const url = "https://aula-17-10-tawny.vercel.app";
 
 
 const getUserAuthenticated = async (user) => {
-  const responseOfApi = await fetch(url + "/user/authenticate",
+  const responseOfApi = await fetch(url + "/user/authenticated",
      {
         method: "POST",
         headers: { "Content-Type": "application/json"},
@@ -15,8 +15,10 @@ const getUserAuthenticated = async (user) => {
 }//receber o usuario
 
 
-const getUsers = () => {
-   
+const getUsers = async () => {
+    const responseOfApiGET = await fetch(url + "/users");
+    const listaDeUsuarios = await responseOfApiGET.json(); //formatando a informação p/ formato json
+    return listaDeUsuarios;
 }
 
 
