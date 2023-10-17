abce1b9e4753ebdc09e8f464f5adf988d61be98f
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-10-17 15:44:10 -0300

    const url criada e const responseOfApi recebendo um EndPoint

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,8 +1,9 @@
 'use server'
+const url = "https://aula-17-10-tawny.vercel.app";
 
 
-const getUserAuthenticated = (userLogin) => {
-  
+const getUserAuthenticated = async (user) => {
+  const responseOfApi = await fetch(url + "/user/authenticate");
 }//receber o usuario
 
 
