7eb835b50d812256cd0b1e5ecca20bc15b678fe7
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-10-17 15:37:14 -0300

    apagando lista de usuários

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,45 +1,14 @@
 'use server'
 
-const lista = [
-    {
-        name: "Anne",
-        email: "anne@gmail.com",
-        password: "123",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-
-    },
-    {
-        name: "Cordelia",
-        email: "Cordelia@gmail.com",
-        password: "456",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-
-    },
-    {
-        name: "Jorge",
-        email: "Jorge@gmail.com",
-        password: "56785978",
-        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" //informando um token válido pela JWT
-
-    }
-]
 
 const getUserAuthenticated = (userLogin) => {
-    let userAutenticado = {};
-
-    {
-        lista.map(user => {
-            if (userLogin.email === user.email && userLogin.password === user.password) {
-                userAutenticado = user;
-            }
-        })
-        return userAutenticado;
-
-    }
+  
 }//receber o usuario
 
 
 const getUsers = () => {
-    return lista;
+   
 }
+
+
 export { getUsers, getUserAuthenticated };
