0cebcced6c01f520a061b80a5adfa04a8ca6ddd9
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-10-31 09:36:01 -0300

    jwtVerify instalada e aplicada na validação do Token

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -1,16 +1,22 @@
-import { decode } from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
-const validateToken = (token) => {
-    const isTokenValidate = decode(token);
-    if (isTokenValidate) {
-        return token = true
-    }
-    return token = false;
+const validateToken = async (token) => {
+    const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTY5MTU0MiwiaWF0IjoxNjk1NjkxNTQyfQ.EG-A9n08TjSSm2affCX5Bl3yRpDbIytMIO5j2iMalLA"
+    try{
+        const isTokenValidate = await jwtVerify (token,
+            new TextEncoder().encode(secret));
+            if (isTokenValidate) {
+                return true;
+            }
+    } catch{
+    return false;
 }
+}
+
 export { validateToken };
 
 /*
-linha 1: lib instalada: jwt
+linha 1: lib instalada: jwtVerify
 linha 4: verificando se o token é válido, ou seja, se foi criado pela JWT
 linha 6: retorna verdadeira caso exista um token
 linha 10: exportando a função criada
