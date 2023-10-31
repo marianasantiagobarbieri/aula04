58bede79fe832bc4b5d285a08eb4d0d6481d6770
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-10-31 10:09:18 -0300

    tratamento do middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,11 @@ import { NextResponse } from "next/server";
 import { validateToken } from "./app/functions/validateToken";
 import handlerAcessUser from "./app/functions/handlerAcess";
 
-export const middleware = (request) => {
+export const middleware = async (request) => {
   const token = request.cookies.get("token")?.value; //pegando o valor do cookie c/ nome token
   const urlLogin = new URL("/", request.url); //acessando a rota Login
   const urlDashboard = new URL("/pages/dashboard", request.url); //acessando a rota Alterar e Registrar
-  const isTokenValidated = validateToken(token); // informando o token e verificando se é válido
+  const isTokenValidated = await validateToken(token); // informando o token e verificando se é válido
 
   if (!isTokenValidated || !token) {
     //se ñ tiver um retorno da verificação for falso ou se ñ tiver token
@@ -19,6 +19,10 @@ export const middleware = (request) => {
       return NextResponse.redirect(urlLogin); // usuário redirecionado p/ a página de Login/Home
     }
   } // resumo: se ñ tiver um token validado ou até mesmo se ñ tiver um token o usuário vai p/ a página Login/Home
+
+  if(isTokenValidated) {
+    return NextResponse.redirect(urlDashboard);
+  }
   NextResponse.next(); //Se o houver um cookie token, o servidor permite prosseguir: indica p/ continuar a aoperação que foi solicitada pelo cliente
 };
 
