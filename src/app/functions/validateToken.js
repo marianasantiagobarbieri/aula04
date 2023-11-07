687c986b18171b71312b96675893001ecf653847
marianasantiagobarbieri <mariana.barbieri@estudante.ifms.edu.br>
2023-11-07 15:57:16 -0300

    Update validateToken.js

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -3,11 +3,12 @@ import { jwtVerify } from "jose";
 const validateToken = async (token) => {
     const secret = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5NTY5MTU0MiwiaWF0IjoxNjk1NjkxNTQyfQ.EG-A9n08TjSSm2affCX5Bl3yRpDbIytMIO5j2iMalLA";
     try{
-        const isTokenValidate = await jwtVerify (token,
+       /* const isTokenValidate = await jwtVerify (token,
             new TextEncoder().encode(secret));
             if (isTokenValidate) {
                 return true;
             }
+            return true*/
     } catch{
     return false;
 }
